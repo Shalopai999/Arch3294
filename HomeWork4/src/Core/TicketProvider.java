c61057f004165627880455455705d1a3bfcee180
Andrey Nepomnyaschikh <And-dk@mail.ru>
2023-08-02 23:25:26 +0700

    seminar4

---
+++ HomeWork4/src/Core/TicketProvider.java
@@ -0,0 +1,43 @@
+package Core;
+
+import Interfaces.ITicketRepo;
+import Models.Ticket;
+import Services.TicketRepository;
+
+/**
+ * Класс - провайдер для работы с базой данных билетов
+ */
+public class TicketProvider {
+
+    private ITicketRepo ticketRepo;
+    public TicketProvider() {
+        // Класс репозитория находится в единственном экземпляре для того, чтобы не создавать несколько подключений
+        // к базе данных. Реализация паттерна Синглтон.
+        this.ticketRepo = TicketRepository.getTicketRepository();
+    }
+
+
+
+    /**
+     * Метод получения билетов из базы данных
+     *
+     * @param routeNumber номер маршрута
+     * @return список билетов
+     * @throws RuntimeException
+     */
+    public Ticket getTickets(int routeNumber) throws RuntimeException {
+        return getTickets(routeNumber);
+    }
+
+
+
+    /**
+     * Метод обновления статуса билета
+     *
+     * @param ticket билет
+     * @return результат выполнения операции
+     */
+    public boolean updateTicketStatus(Ticket ticket) {
+        return true;
+    }
+}
